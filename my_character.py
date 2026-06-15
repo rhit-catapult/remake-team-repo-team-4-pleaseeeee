8d742f8a41522deb04d183cde5ff0d9187aa87f3
pjun24315656-jpg <pjun24315656@gmail.com>
2026-06-15 11:07:23 -0400

    Update my_character.py

--- my_character.py
+++ my_character.py
@@ -1,17 +1,23 @@
 import pygame
 import sys
 
+
 class Character:
-    def __init__(self, screen: pygame.Surface, x, y):
+    def __init__(self, screen, x, y):
         self.screen = screen
         self.x = x
         self.y = y
+        self.image = pygame.image.load("brownie.png")
+
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
 
 
 # This function is called when you run this file, and is used to test the Character class individually.
 # When you create more files with different classes, copy the code below, then
-# change it to properly test that class
+# change it to properly test that class.
 def test_character():
+    pygame.init()
     # TODO: change this function to test your class
     screen = pygame.display.set_mode((640, 480))
     character = Character(screen, 400, 400)
